dd4206dbe15ba7759bcb3a9e67797919996c2688
Rohit Raman Gopalakrishnan <rohit.gopalakrishnan@publicissapient.com>
2020-06-06 11:54:18 +0530

    Changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'testApp';
+export class AppComponent implements AfterViewInit{
+  response:any;
+  constructor(private http:HttpClient){
+
+  }
+  ngAfterViewInit(): void {
+
+    this.http.get("https://testangularbackend.azurewebsites.net/api/test")
+              .subscribe((obj)=>{
+                  this.response=obj;
+              })
+    }
+
+
 }
